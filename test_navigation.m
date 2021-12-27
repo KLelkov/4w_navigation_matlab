fed67d5531110b54245846b08381b774f8bead1d
pec-orange <pec-orange@mail.ru>
2021-12-27 17:11:29 +0300

    [feature] lowered solution frequency to 40 Hz to match onboard ROS node

--- test_navigation.m
+++ test_navigation.m
@@ -1,16 +1,18 @@
 function [X, Y, Heading] = test_navigation(time, gamma1, gamma2, Gyro, Lat, Lon, Vn, Ve, w1, w2, w3, w4)
 
 len = length(time);
+len2 = floor(len/5);
     referenceNeeded = true;
     notMovedYet = true;
     validCoords = false;
     sensors.dt = 0.01;
     referenceLat = 0;
     referenceLon = 0;
-    X = zeros(len, 1);
-    Y = zeros(len, 1);
-    Heading = zeros(len, 1);
-    Anr = zeros(len, 1);
+    X = zeros(len2, 1);
+    Y = zeros(len2, 1);
+    Heading = zeros(len2, 1);
+    Anr = zeros(len2, 1);
+    time2 = zeros(len2, 1);
     
     
     % init sensors struct
@@ -33,11 +35,14 @@ len = length(time);
     kalman_state.dh = 1e-1;
     kalman_state.odo = 3e-2;
     
-    
+    cnt = 0;
     
     for i = 1:len
-        if i > 1
-            sensors.dt = (time(i) - time(i-1)) / 1000;
+        if rem(i, 5) == 0
+            cnt = cnt +1;
+            time2(cnt) = time(i);
+        if i > 5
+            sensors.dt = (time(i) - time(i-5)) / 1000;
         end
         % EDIT upper limit!
         if w1(i) > 0.2 && w1(i) < 15
@@ -111,11 +116,11 @@ len = length(time);
 %         update.gyro = 0;
         
         kalman_state = ukfNav(kalman_state, sensors, update);
-        X(i) = kalman_state.X(1);
-        Y(i) = kalman_state.X(2);
-        Heading(i) = wrapToPi(kalman_state.X(3));
-        Anr(i) = kalman_state.X(5);
-        
+        X(cnt) = kalman_state.X(1);
+        Y(cnt) = kalman_state.X(2);
+        Heading(cnt) = wrapToPi(kalman_state.X(3));
+        Anr(cnt) = kalman_state.X(5);
+        end
     end
     close all
     
@@ -133,39 +138,40 @@ len = length(time);
     
     
     figure;
-    plot(time, Heading, 'b')
+    plot(time2, Heading, 'b')
     hold on;
     grid on
-    plot(time, Head, 'k');
+%     plot(time2, Head, 'k');
     legend filter raw
+    title 'Heading'
     
     figure;
-    plot(time, Anr, 'b')
+    plot(time2, Anr, 'b')
     hold on;
     grid on
-    plot(time, Rot, 'k');
-    plot(time, -Gyro, 'r');
+%     plot(time2, Rot, 'k');
+%     plot(time2, -Gyro, 'r');
     legend filter raw gyro
     
-    figure;
-    plot(time, Anr, 'b')
-    hold on;
-    grid on
-    plot(time, gamma1*pi/180, 'r');
-    plot(time, gamma2*pi/180, 'g');
-    plot(time, -Gyro, 'k');
-    legend filter_anr gamma1 gamma2 gyro
+%     figure;
+%     plot(time2, Anr, 'b')
+%     hold on;
+%     grid on
+%     plot(time2, gamma1*pi/180, 'r');
+%     plot(time2, gamma2*pi/180, 'g');
+%     plot(time2, -Gyro, 'k');
+%     legend filter_anr gamma1 gamma2 gyro
     
-    figure('Name', 'Localization Error');
-    Err = zeros(len, 1);
-    for i = 1 : len
-        Ey = Y(i) - East(i);
-        Ex = X(i) - North(i);
-        Err(i) = sqrt(Ey^2 + Ex^2);
-    end
+%     figure('Name', 'Localization Error');
+%     Err = zeros(len, 1);
+%     for i = 1 : len
+%         Ey = Y(i) - East(i);
+%         Ex = X(i) - North(i);
+%         Err(i) = sqrt(Ey^2 + Ex^2);
+%     end
     
-    plot(time, Err, 'b', 'LineWidth', 2)
-    grid on
+%     plot(time2, Err, 'b', 'LineWidth', 2)
+%     grid on
     
     
     [Xloc, Yloc] = ned2local(X, Y, offset);
